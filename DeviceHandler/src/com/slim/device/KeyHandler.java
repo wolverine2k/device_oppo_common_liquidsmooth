6e5515c848f87380b4b62b0df8c724d56ac6bef2
wolverine2k <nareshtechs@gmail.com>
2015-12-22 15:54:30 +0100

    Modifyed OPPO Device Common for LiquidSmooth

---
+++ DeviceHandler/src/com/slim/device/KeyHandler.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slim.device;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.slim.device.settings.ScreenOffGesture;
+
+import com.android.internal.os.DeviceKeyHandler;
+import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.slim.ActionConstants;
+import com.android.internal.util.slim.Action;
+
+public class KeyHandler implements DeviceKeyHandler {
+
+    private static final String TAG = KeyHandler.class.getSimpleName();
+    private static final int GESTURE_REQUEST = 1;
+
+    // Supported scancodes
+    private static final int GESTURE_V_UP_SCANCODE = 249;
+    private static final int GESTURE_CIRCLE_SCANCODE = 250;
+    private static final int GESTURE_SWIPE_DOWN_SCANCODE = 251;
+    private static final int GESTURE_V_SCANCODE = 252;
+    private static final int GESTURE_LTR_SCANCODE = 253;
+    private static final int GESTURE_GTR_SCANCODE = 254;
+    private static final int KEY_DOUBLE_TAP = 255;
+
+    private static final int[] sSupportedGestures = new int[]{
+        GESTURE_CIRCLE_SCANCODE,
+        GESTURE_SWIPE_DOWN_SCANCODE,
+        GESTURE_V_SCANCODE,
+        GESTURE_V_UP_SCANCODE,
+        GESTURE_LTR_SCANCODE,
+        GESTURE_GTR_SCANCODE,
+        KEY_DOUBLE_TAP
+    };
+
+    private final Context mContext;
+    private final PowerManager mPowerManager;
+    private Context mGestureContext = null;
+    private EventHandler mEventHandler;
+    private SensorManager mSensorManager;
+    private Sensor mProximitySensor;
+    WakeLock mProximityWakeLock;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+        mEventHandler = new EventHandler();
+        mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mProximityWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "ProximityWakeLock");
+
+        try {
+            mGestureContext = mContext.createPackageContext(
+                    "com.slim.device", Context.CONTEXT_IGNORE_SECURITY);
+        } catch (NameNotFoundException e) {
+        }
+    }
+
+    private class EventHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            KeyEvent event = (KeyEvent) msg.obj;
+            String action = null;
+            switch(event.getScanCode()) {
+            case GESTURE_CIRCLE_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_CIRCLE,
+                        ActionConstants.ACTION_CAMERA);
+                break;
+            case GESTURE_SWIPE_DOWN_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_DOUBLE_SWIPE,
+                        ActionConstants.ACTION_MEDIA_PLAY_PAUSE);
+                break;
+            case GESTURE_V_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_DOWN,
+                        ActionConstants.ACTION_VIB_SILENT);
+                break;
+            case GESTURE_V_UP_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_UP,
+                        ActionConstants.ACTION_TORCH);
+                break;
+            case GESTURE_LTR_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_LEFT,
+                        ActionConstants.ACTION_MEDIA_PREVIOUS);
+                break;
+            case GESTURE_GTR_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_RIGHT,
+                        ActionConstants.ACTION_MEDIA_NEXT);
+                break;
+            case KEY_DOUBLE_TAP:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_DOUBLE_TAP,
+                        ActionConstants.ACTION_WAKE_DEVICE);
+                break;
+            }
+            if (action == null || action != null && action.equals(ActionConstants.ACTION_NULL)) {
+                return;
+            }
+            if (action.equals(ActionConstants.ACTION_CAMERA)
+                    || !action.startsWith("**")) {
+                Action.processAction(mContext, ActionConstants.ACTION_WAKE_DEVICE, false);
+            }
+            Action.processAction(mContext, action, false);
+        }
+    }
+
+    private SharedPreferences getGestureSharedPreferences() {
+        return mGestureContext.getSharedPreferences(
+                ScreenOffGesture.GESTURE_SETTINGS,
+                Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS);
+    }
+
+    public boolean handleKeyEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_UP) {
+            return false;
+        }
+        int scanCode = event.getScanCode();
+        boolean isKeySupported = ArrayUtils.contains(sSupportedGestures, scanCode);
+        if (isKeySupported && !mEventHandler.hasMessages(GESTURE_REQUEST)) {
+            Message msg = getMessageForKeyEvent(event);
+            if (mProximitySensor != null) {
+                mEventHandler.sendMessageDelayed(msg, scanCode == KEY_DOUBLE_TAP ? 400 : 200);
+                processEvent(event);
+            } else {
+                mEventHandler.sendMessage(msg);
+            }
+        }
+        return isKeySupported;
+    }
+
+    private Message getMessageForKeyEvent(KeyEvent keyEvent) {
+        Message msg = mEventHandler.obtainMessage(GESTURE_REQUEST);
+        msg.obj = keyEvent;
+        return msg;
+    }
+
+    private void processEvent(final KeyEvent keyEvent) {
+        mProximityWakeLock.acquire();
+        mSensorManager.registerListener(new SensorEventListener() {
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                mProximityWakeLock.release();
+                mSensorManager.unregisterListener(this);
+                if (!mEventHandler.hasMessages(GESTURE_REQUEST)) {
+                    // The sensor took to long, ignoring.
+                    return;
+                }
+                mEventHandler.removeMessages(GESTURE_REQUEST);
+                if (event.values[0] == mProximitySensor.getMaximumRange()) {
+                    Message msg = getMessageForKeyEvent(keyEvent);
+                    mEventHandler.sendMessage(msg);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+        }, mProximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+}
